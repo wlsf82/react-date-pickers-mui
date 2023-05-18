524116bb14d8e602e12670061fabfd69cc993ade
Walmyr <wlsf82@gmail.com>
2023-05-18 02:41:53 +0200

    Integrate project with Cypress Cloud
    And add new badge to the readme file.

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '2obqu6',
   e2e: {
     baseUrl: 'https://mui.com/x/react-date-pickers',
     env: { hideXhr: true },

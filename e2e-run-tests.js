0f5b7e5ee1fa7e39e7a764f5dec6ac6663a493c0
Walmyr <wlsf82@gmail.com>
2023-06-08 01:10:50 +0200

    Allow running tests through node
    And create a json report of their execution.

---
+++ e2e-run-tests.js
@@ -0,0 +1,58 @@
+const cypress = require('cypress')
+const fs = require('fs')
+const path = require('path')
+
+cypress.run({ reporter: null }).then(({
+  status,
+  startedTestsAt,
+  endedTestsAt,
+  totalDuration,
+  totalTests,
+  totalPassed,
+  totalPending,
+  totalFailed,
+  totalSkipped,
+  browserName,
+  browserVersion,
+  osName,
+  osVersion,
+  cypressVersion,
+  runs,
+}) => {
+  let testResultsObj = {
+    status,
+    startedTestsAt,
+    endedTestsAt,
+    totalDuration: `${totalDuration / 1000} seconds`,
+    totalTests,
+    totalPassed,
+    totalPending,
+    totalFailed,
+    totalSkipped,
+    browserName,
+    browserVersion,
+    osName,
+    osVersion,
+    cypressVersion,
+  }
+
+  const testCases = runs[0].tests.map((({ title }) => title[title.length - 1]))
+
+  testResultsObj.testCases = testCases
+  testResultsObj = JSON.stringify(testResultsObj)
+
+  const testReportDir = path.resolve(`${__dirname}/test-report`)
+
+  if (!fs.existsSync(testReportDir)){
+    fs.mkdirSync(testReportDir)
+  }
+
+  const testResultsFile = `${testReportDir}/testResults.json`
+
+  fs.writeFile(testResultsFile, testResultsObj, err => {
+    if (err) {
+      console.error(err)
+    }
+    console.log(`\nTest report available at: ${testResultsFile}\n`)
+  })
+})

f38ff82a4ccd05eba84f40818aa4bdf8a74f655c
Walmyr <wlsf82@gmail.com>
2023-06-07 04:22:42 +0200

    Remove import of deleted file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import * as React from 'react'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import './App.css'
 
 export default function FirstComponent() {
   return (
